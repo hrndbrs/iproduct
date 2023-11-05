b69236b79eac534db9fe8eded92845d8bd633949
hrndbrs <hrndbrs@gmail.com>
2023-11-05 16:59:01 +0700

    test

--- src/app/config/api.js
+++ src/app/config/api.js
@@ -1,6 +1,6 @@
 export default async function client(endpoint, method = "GET", body) {
     // const baseUrl = "http://localhost:3001/pub"
-    const baseUrl = "https://p3c1-server.hrndbrs.my.id"
+    const baseUrl = "https://p3c1-server.hrndbrs.my.id/pub"
 
 
     const config = {
